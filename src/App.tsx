618a78ec837074cede63a299885aea90ac6c5392
CIPHERTron <pritish.samal918@gmail.com>
2023-02-24 19:04:58 +0530

    feat: integrate tailwind with tw and styled components
    Signed-off-by: CIPHERTron <pritish.samal918@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+import tw from 'twin.macro';
+
+const Container = styled.div`
+  ${tw`
+  h-60
+  m-2.5
+  rounded-2xl
+  flex
+  flex-col
+  justify-center
+  items-center
+  `};
+`;
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <h1 className="text-3xl font-bold underline text-red-600">
+      Project Tart
+    </h1>
+    </Container>
   );
 }
 
