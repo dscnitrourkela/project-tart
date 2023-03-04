03d679e6cc82d503887db9326bb322df51c5bb29
Anish Sarawgi <98693953+Anish-Sarawgi@users.noreply.github.com>
2023-03-04 14:45:40 +0530

    feat: add footer (#7)
    * feat: add footer
    
    * fix: typo in workflows dir name
    
    Signed-off-by: CIPHERTron <pritish.samal918@gmail.com>
    
    ---------
    
    Signed-off-by: CIPHERTron <pritish.samal918@gmail.com>
    Co-authored-by: CIPHERTron <pritish.samal918@gmail.com>

--- src/Constants/Constants.ts
+++ src/Constants/Constants.ts
@@ -20,3 +20,29 @@ export const FAQs = [
 		answer: 'Innovision is a fest',
 	},
 ];
+
+export const footer = {
+	Logo: {
+		title: 'NITRUTSAV 2023',
+		alt: 'logo',
+	},
+	ImpLinks: {
+		title: 'Quick Links',
+		impLinks: [
+			// { text: 'LOGIN/REGISTER', href: '#' },
+			{ text: 'PRO SHOW', href: '#' },
+			{ text: 'FLAGSHIP EVENTS', href: '#' },
+			{ text: 'MAIN EVENTS', href: '#' },
+			{ text: 'LECTURES & WORKSHOPS', href: '#' },
+			{ text: 'FUN EVENTS', href: '#' },
+			{ text: 'EXHIBITION', href: '#' },
+		],
+	},
+	Timings: {
+		title: 'Opening Hours',
+		weekends: 'Timings',
+		weekdays: 'Timings',
+	},
+
+	CopyRight: 'Designed and Developed with 💙 by GDSC NIT, Rourkela',
+};
