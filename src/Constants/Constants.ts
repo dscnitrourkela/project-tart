a0ac33983f853ca042bd81313829d61fb19faf6b
starkjay21 <starkjay2107@gmail.com>
2023-03-04 13:19:01 +0530

    feat: added navbar
    Signed-off-by: starkjay21 <starkjay2107@gmail.com>

--- src/Constants/Constants.ts
+++ src/Constants/Constants.ts
@@ -1,1 +1,19 @@
 export const FAQs = [];
+
+export const NavListItems = [
+	{
+		index: 1,
+		src: '#',
+		name: 'Events',
+	},
+	{
+		index: 2,
+		src: '#',
+		name: 'Events',
+	},
+	{
+		index: 3,
+		src: '#',
+		name: 'Events',
+	},
+];
