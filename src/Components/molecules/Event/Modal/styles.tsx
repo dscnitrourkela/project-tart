1bc6aa6c75906ffef963df7f6afd887918854825
CIPHERTron <pritish.samal918@gmail.com>
2023-03-05 18:57:46 +0530

    fix: make event modal sticky
    Signed-off-by: CIPHERTron <pritish.samal918@gmail.com>

--- src/Components/molecules/Event/Modal/styles.tsx
+++ src/Components/molecules/Event/Modal/styles.tsx
@@ -4,13 +4,14 @@ import tw from 'twin.macro';
 export const BackDrop = styled.div`
 	z-index: 10;
 	width: 100vw;
+	height: 100vh;
 	background-color: #ffffff55;
 	backdrop-filter: blur(10px);
+	position: sticky;
+	left: 0%;
+	top: 0%;
 	${tw`
     py-6
-    absolute
-    left-0
-    top-0
     flex
     justify-center
     items-center
