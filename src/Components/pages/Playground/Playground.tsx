6337323227d2d1988e81a7af0ebb7f98308e9cd7
CIPHERTron <pritish.samal918@gmail.com>
2023-03-05 16:03:50 +0530

    feat: add nav in playground
    Signed-off-by: CIPHERTron <pritish.samal918@gmail.com>

--- src/Components/pages/Playground/Playground.tsx
+++ src/Components/pages/Playground/Playground.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import { Body1, Body2, Button, Caption, Heading1, Heading2, Heading3, Heading4, NavText } from 'Components/atoms';
-import { Navbar } from 'Components/molecules/Navbar';
 import { Footer } from 'Components/molecules';
+import { Navbar } from 'Components/molecules/Navbar';
 
 const Playground: React.FC = () => {
 	return (
 		<>
+			<Navbar />
 			<span style={{ marginTop: '500px' }}>Hello</span>
 			<Button btnText="Label" filled={true} success={true} />
 			<Heading1>Heading 1</Heading1>
