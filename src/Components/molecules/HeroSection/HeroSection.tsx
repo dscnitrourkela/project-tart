2ef1a469f23eae443b7f646f9108dbcd2c02b204
CIPHERTron <pritish.samal918@gmail.com>
2023-03-06 14:54:50 +0530

    fix: hero buttons
    Signed-off-by: CIPHERTron <pritish.samal918@gmail.com>

--- src/Components/molecules/HeroSection/HeroSection.tsx
+++ src/Components/molecules/HeroSection/HeroSection.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 
-import { Container, Img, ButtonWrapper, SectionContainer } from './styles';
 import { Button } from 'Components/atoms';
+import useMediaQuery from 'Hooks/useMediaQuery';
+
+import { ButtonWrapper, Container, Img, SectionContainer } from './styles';
 
 const HeroSection: React.FC = () => {
+	const isMobileView = useMediaQuery('(max-width: 544px)');
 	return (
 		<>
 			<SectionContainer>
 				<Container>
 					<Img src="NU_Branding.svg" alt="NU"></Img>
 					<ButtonWrapper>
-						<Button btnText="Explore More " />
-						<Button filled btnText="Register Now" />
+						<Button fullWidth={isMobileView} btnText="Explore More " />
+						<Button fullWidth={isMobileView} filled btnText="Register Now" />
 					</ButtonWrapper>
 				</Container>
 			</SectionContainer>
