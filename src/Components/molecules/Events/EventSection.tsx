0b7a86716f4ace5a8f81361a333648b3554fb7b9
Ashish Padhy <100484401+Shurtu-gal@users.noreply.github.com>
2023-03-06 15:02:17 +0530

    fix: categoryCards content and spacing (#28)
    Signed-off-by: Ashish Padhy <100484401+Shurtu-gal@users.noreply.github.com>

--- src/Components/molecules/Events/EventSection.tsx
+++ src/Components/molecules/Events/EventSection.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 
 import Card from 'Components/atoms/CategoryCard/Card';
+import { Events } from 'Constants/Constants';
 
 import { Container } from './styles';
 
 const EventSection: React.FC = () => {
 	return (
 		<Container>
-			{[1, 2, 3, 4].map((key) => (
-				<Card key={key} />
-			))}
+			{Events.map(({ slugName, description, image, title }) => {
+				return <Card key={slugName} description={description} image={image} title={title} link={slugName} />;
+			})}
 		</Container>
 	);
 };
