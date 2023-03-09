f0ed883c7753932fb3576fd9babea5ae7d495f42
Ashish Padhy <100484401+Shurtu-gal@users.noreply.github.com>
2023-03-09 23:34:44 +0530

    feat: add google analytics
    Signed-off-by: Ashish Padhy <100484401+Shurtu-gal@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import './App.css';
 
 import React from 'react';
 
+import ReactGA from 'react-ga';
 import { ToastContainer } from 'react-toastify';
 import Routes from 'Routes';
 import AuthContextProvider from 'utils/AuthContext';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const TRACKING_ID = 'G-SL8G6BBX7L';
+ReactGA.initialize(TRACKING_ID);
 
 const queryClient = new QueryClient();
 
