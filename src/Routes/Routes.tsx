04e261dd5eecb70a8133451223fc3a7b981bc748
Pritish Samal <pritish.samal918@gmail.com>
2023-03-17 13:23:44 +0530

    feat: close registrations (#79)
    Signed-off-by: CIPHERTron <pritish.samal918@gmail.com>

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -6,10 +6,10 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 const Homepage = React.lazy(() => import('Components/pages/Home'));
 const Playground = React.lazy(() => import('Components/pages/Playground'));
 const EventPage = React.lazy(() => import('Components/pages/Event'));
-const Register = React.lazy(() => import('Components/pages/Register'));
+// const Register = React.lazy(() => import('Components/pages/Register'));
 const ProfilePage = React.lazy(() => import('Components/pages/Profile'));
 const PageNotFound = React.lazy(() => import('Components/pages/PageNotFound'));
-const TxnSuccessScreen = React.lazy(() => import('Components/pages/TxnSuccessScreen'));
+const RegistrationsClosed = React.lazy(() => import('Components/pages/RegistrationsClosed'));
 
 const Routes: React.FC = () => {
 	return (
@@ -20,9 +20,9 @@ const Routes: React.FC = () => {
 					<Route exact path="/" component={Homepage} />
 					<Route path="/event/:eventName" component={EventPage} />
 					<Route exact path="/playground" component={Playground} />
-					<Route exact path="/register" component={Register} />
+					<Route exact path="/register" component={RegistrationsClosed} />
 					<Route exact path="/profile" component={ProfilePage} />
-					<Route exact path="/txn-success" component={TxnSuccessScreen} />
+					<Route exact path="/closed" component={RegistrationsClosed} />
 					<Route exact path="/loader" component={Loader} />
 					<Route exact path="*" component={PageNotFound} />
 				</Switch>
